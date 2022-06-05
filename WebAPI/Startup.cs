78dee22c72810eed8fae4772e51f90298666849d
Berat Yeşbek <beratyesbek@hotmail.com>
2022-06-05 23:50:30 +0300

    heroku deploy

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -67,12 +67,11 @@ namespace WebAPI
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
-            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+
 
             app.UseHttpsRedirection();
 
