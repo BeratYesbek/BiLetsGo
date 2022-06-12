7ca7404f3d1a747b42f7b9d5cf85c9f62bf2a725
Berat Yeşbek <beratyesbek@hotmail.com>
2022-06-12 09:50:46 +0300

    fix

--- Business/Validation/TicketValidator.cs
+++ Business/Validation/TicketValidator.cs
@@ -15,8 +15,8 @@ namespace Business.Validation
             RuleFor(t => t.CategoryId).NotEmpty();
             RuleFor(t => t.EventFinishedDate).NotEmpty();
             RuleFor(t => t.EventStartedDate).NotEmpty();
-            RuleFor(t => t.Title).NotEmpty().Length(10);
-            RuleFor(t => t.Description).NotEmpty().Length(50);
+            RuleFor(t => t.Title).NotEmpty().MinimumLength(10);
+            RuleFor(t => t.Description).NotEmpty().MinimumLength(50);
             RuleFor(t => t.Price).NotEmpty().GreaterThan(0);
             RuleFor(t => t.Quantity).NotEmpty().GreaterThan(0);
             RuleFor(t => t.SalonId).NotEmpty();
